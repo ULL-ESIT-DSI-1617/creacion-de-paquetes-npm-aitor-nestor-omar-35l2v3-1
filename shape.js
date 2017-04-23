7ed281758dc366929fea2296d3be0ee4ffb65b18
Aitor Bernal <alu0100830064@ull.edu.es>
2017-04-23 11:42:33 +0100

    subiendo la clase shape

---
+++ shape.js
@@ -0,0 +1,15 @@
+let Shapes = {};
+class Figura {
+
+    constructor(shapes,options) {
+        this.shapes = shapes;
+        this.options = options
+    }
+
+    getArea() {
+        let shape = new Shapes[this.shapes](this.options)
+        let area = shape.getArea()
+        return area;
+    }
+
+}
